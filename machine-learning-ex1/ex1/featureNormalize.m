cc6e34d1d1b274d5cdbbeeb4174911e87d0d7cb9
BlueBlueSkyZZ <493813966@qq.com>
2018-11-12 11:05:20 +0800

    mulitple linear regression finished

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -25,9 +25,13 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %
-mu = mean(X(:,));
-sigma = std(X(:,));
-X = (X - mu)/sigma;
+mu = mean(X_norm);
+sigma = std(X_norm);
+for i = 1:size(X, 2)
+  X_norm(:, i) = X_norm(:, i) - mu(i);
+  X_norm(:, i) = X_norm(:, i) / sigma(i);
+end
+
 
 
 
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -11,12 +11,14 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+    hx = X * theta;
+    temp(:, iter) = theta - alpha / m * (X' * (hx - y));
+    theta = temp(:, iter);
 
 
 
@@ -29,7 +31,7 @@ for iter = 1:num_iters
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
